2f882e3ab3efa02d00778e60e92aa61ad2d4d798
Scott Smith <scthsm@outlook.com>
2020-04-10 09:19:59 -0600

    a

---
+++ routerActions/actionRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const Actions = require('../data/helpers/actionModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Actions.get(req.query)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the actions',
+      });
+    });
+  });
