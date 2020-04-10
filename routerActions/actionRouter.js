a2ad52ed7dac86f1bb0f88496143bf1d6172e249
Scott Smith <scthsm@outlook.com>
2020-04-10 10:15:42 -0600

    a

--- routerActions/actionRouter.js
+++ routerActions/actionRouter.js
@@ -18,5 +18,45 @@ router.get('/:id', (req, res) => {
     });
   });
 
+  router.delete('/:id', (req, res) =>  {
+    Actions.remove(req.params.id)
+    .then(action => {
+      if (action.length == 0) {
+        res.status(500).json({
+          message: "No Action Found"
+        });
+      } else {
+        res.status(200).json({
+          message: "Action deleted"
+        });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the Action',
+      });
+    });
+  });
+
+  router.put('/:id', (req, res) =>  {
+    const changes = req.body;
+    Actions.update(req.params.id,changes)
+    .then(() => {
+      if (!changes.description || !changes.notes) {
+        res.status(400).json({ errorMessage: "Text field can't be empty" });
+      } else {
+        res.status(200).json({ message: `action updated.` });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the action',
+      });
+    });
+  })
 
   module.exports = router;
